3af2824b367b77ce8b6c86a6681d5f52028d6ab5
Rosesh Chauhan <chauhanrosesh9@gmail.com>
2026-01-18 19:42:52 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,30 @@
+import sys
+import logging
+
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    return (
+        f"Error occurred in python script "
+        f"name [{file_name}] "
+        f"line number [{exc_tb.tb_lineno}] "
+        f"error message [{error}]"
+    )
+
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail: sys):
+        super().__init__(str(error))
+        self.error_message = error_message_detail(error, error_detail)
+
+    def __str__(self):
+        return self.error_message    
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Division by zero encountered")
+        raise CustomException(e, sys) from e
